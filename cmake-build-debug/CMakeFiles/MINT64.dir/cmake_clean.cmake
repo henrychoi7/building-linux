6767296f89eadb97fcbf4c7348fa9bddd5f94601
henrychoi7 <wschd770@gmail.com>
2017-08-31 18:19:00 +0900

    added all files

---
+++ cmake-build-debug/CMakeFiles/MINT64.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MINT64"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/MINT64.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

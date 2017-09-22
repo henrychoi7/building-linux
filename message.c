aa9597012bfe20e443808d1f9a756c86895ccfd3
henrychoi7 <wschd770@gmail.com>
2017-09-22 21:19:50 +0900

    [BootLoader] Build completed with text output in the screen
    Signed-off-by: henrychoi7 <wschd770@gmail.com>

---
+++ message.c
@@ -0,0 +1,22 @@
+int main() {
+    int i = 0;
+    int j = 0;
+    char* pcVideoMemory = ( char* ) 0xB8000;
+    char* pcMessage = "MINT64 OS Boot Loader Start~!!";
+    char cTemp;
+    
+    while ( 1 )
+    {
+        cTemp = pcMessage[ i ];
+    
+        if( cTemp == 0 )
+        {
+            break;
+        }
+    
+        pcVideoMemory[ j ] = cTemp;
+        i += 1;
+        j += 2;
+    }
+    return 0;
+}
